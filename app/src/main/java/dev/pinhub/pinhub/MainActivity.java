601d60a1f2513df7c5b9c425a2f0c602706f4b23
Donatas T <donatas.tamosauskas@gmail.com>
2018-09-17 21:58:22 +0300

    Added simple switching to the new activity.

--- app/src/main/java/dev/pinhub/pinhub/MainActivity.java
+++ app/src/main/java/dev/pinhub/pinhub/MainActivity.java
@@ -1,7 +1,10 @@
 package dev.pinhub.pinhub;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        discountedListButtonListenerCreator();
     }
 
 
@@ -42,5 +47,21 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(54.674886, 25.273520);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void discountedListButtonListenerCreator() {
+        Button discountedList = findViewById(R.id.DiscountedListActivityButton);
+
+        discountedList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchToDiscountedListActivity();
+            }
+        });
+    }
+
+    private void switchToDiscountedListActivity() {
+        Intent discountedProductListActivity = new Intent(this, DiscountedProductListActivity.class);
+        startActivity(discountedProductListActivity);
     }
 }
